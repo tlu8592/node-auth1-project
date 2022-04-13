98f707bde131782ddf132b1057bdccb6a00c87ef
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-12 20:03:11 -0400

    model functions written

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,40 @@
+const db = require('../../data/db-config');
+
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+  // SELECT user_id AND username FROM users
+  db.select(['user_id', 'username']).from('users');
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-
+  db.select()
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  db.select(['user_id', 'username'])
+    .from('users')
+    .where('user_id', user_id);
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
 function add(user) {
-
+  db('users').insert(user)
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add
+}
